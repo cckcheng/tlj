211ab5ba987f36f86f88d105592725c71ba491ae
ccheng <colley888@gmail.com>
2019-02-11 17:36:14 -0800

    add help

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var http = require('http');
+var fs = require('fs');
+
+fs.readFile('TLJHelp.html', function(err, data) {
+  http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  }).listen(8088);
+});
+
 
 
